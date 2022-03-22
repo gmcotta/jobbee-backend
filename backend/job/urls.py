6990590cf42c951b4bbeaf58414dacd2263e8f47
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-22 18:18:03 -0300

    fix: change order of create job route

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('jobs/', views.getAllJobs, name='jobs'),
-    path('jobs/<str:id>/', views.getJobById, name='job'),
     path('jobs/new/', views.createJob, name='new_job'),
+    path('jobs/<str:id>/', views.getJobById, name='job'),
     path('jobs/<str:id>/update/', views.updateJob, name='update_job'),
     path('jobs/<str:id>/delete/', views.deleteJob, name='delete_job'),
 ]
