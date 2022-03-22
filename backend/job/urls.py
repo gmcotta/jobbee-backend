c4a8fe928aea2f34a4716d23855359119040bc36
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-22 17:33:32 -0300

    feat(job): create route for get job by id

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jobs/', views.getAllJobs, name='jobs')
+    path('jobs/', views.getAllJobs, name='jobs'),
+    path('jobs/<str:id>/', views.getJobById, name='job'),
 ]
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -11,4 +11,10 @@ from .models import Job
 def getAllJobs(request):
     jobs = Job.objects.all()
     serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getJobById(request, id):
+    job = get_object_or_404(Job, id=id)
+    serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
