4d9beb800da3aa5ff03dcc1b0117279707e2163d
Gustavo Cotta <gmcotta34@gmail.com>
2022-03-22 18:02:20 -0300

    feat(job): create routes for add job and update job

--- backend/job/views.py
+++ backend/job/views.py
@@ -18,3 +18,33 @@ def getJobById(request, id):
     job = get_object_or_404(Job, id=id)
     serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def createJob(request):
+    data = request.data
+    job = Job.objects.create(**data)
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateJob(request, id):
+    job = get_object_or_404(Job, id=id)
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+    # job.point = request.data['point']
+    # job.lastDate = request.data['lastDate']
+    # job.user = request.data['user']
+    # job.createdAt = request.data['createdAt']
+
+    job.save()
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
